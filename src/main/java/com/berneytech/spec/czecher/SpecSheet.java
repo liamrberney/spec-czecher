41bef4bc74b83b3e03ee80fd18cda93e196e698a
Liam Berney <liamrberney@gmail.com>
2018-10-05 07:59:55 -0500

    Initial Commit

---
+++ src/main/java/com/berneytech/spec/czecher/SpecSheet.java
@@ -0,0 +1,16 @@
+package com.berneytech.spec.czecher;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author bernelia000
+ */
+public class SpecSheet {
+
+    
+}
